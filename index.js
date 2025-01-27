fb048378e25a84c2e97685a90d7715f5e58ffcb0
fuerte <leandrofuerte75@gmail.com>
2025-01-27 10:30:51 +0100

    Adding new user

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ const prisma = new PrismaClient();
 async function main() {
   const newUser = await prisma.user.create({
     data: {
-      email: "marcos@gmail.com",
-      name: "Marcos",
-      lastname: "Perez",
+      email: "joe@gmail.com",
+      name: "Joe",
+      lastname: "Doe",
     },
   });
 
