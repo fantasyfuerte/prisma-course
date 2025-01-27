94ca7394610fb765fd7a63e5bae2e0df57f3f5c5
fuerte <leandrofuerte75@gmail.com>
2025-01-27 09:28:17 +0100

    Adding new user and type module to package json

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ const prisma = new PrismaClient();
 async function main() {
   //   const newUser = await prisma.user.create({
   //     data: {
-  //       email: "maria@gmail.com",
-  //       name: "Maria",
-  //       lastname: "Rodriguez",
+  //       email: "leo@gmail.com",
+  //       name: "Leonardo",
+  //       lastname: "Rossi",
   //     },
   //   });
   //   console.log(newUser);
@@ -17,7 +17,7 @@ async function main() {
 
   const user = await prisma.user.findFirst({
     where: {
-      name: "Maria",
+      name: "Leonardo",
     },
   });
   console.log(user.email + " - " + user.name + " - " + user.lastname);
