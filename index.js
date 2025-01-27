6955cff8aa5c8535f707ca68115683ce817b4684
fuerte <leandrofuerte75@gmail.com>
2025-01-27 10:20:23 +0100

    using delete

--- index.js
+++ index.js
@@ -3,31 +3,42 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 async function main() {
-  //   const newUser = await prisma.user.create({
-  //     data: {
-  //       email: "leo@gmail.com",
-  //       name: "Leonardo",
-  //       lastname: "Rossi",
-  //     },
-  //   });
-  //   console.log(newUser);
+  // const newUser = await prisma.user.create({
+  //   data: {
+  //     email: "leo@gmail.com",
+  //     name: "Leonardo",
+  //     lastname: "Rossi",
+  //   },
+  // });
 
-  //   const users = await prisma.user.findMany();
-  //   console.log(users);
+  // console.log(newUser);
 
-  const user = await prisma.user.findFirst({
+  // const users = await prisma.user.findMany();
+
+  // console.log(users);
+
+  // const user = await prisma.user.findFirst({
+  //   where: {
+  //     AND: [
+  //       {
+  //         email: "leo@gmail.com",
+  //       },
+  //       {
+  //         name: "Leonardo",
+  //       },
+  //     ],
+  //   },
+  // });
+
+  // console.log(user.email + " - " + user.name + " - " + user.lastname);
+
+  const user = await prisma.user.delete({
     where: {
-      AND: [
-        {
-          email: "leo@gmail.com",
-        },
-        {
-          name: "Leonardo",
-        },
-      ],
+      email: "leo@gmail.com",
     },
   });
-  console.log(user.email + " - " + user.name + " - " + user.lastname);
+
+  console.log(user); //devuelve el eliminado
 }
 
 main();
