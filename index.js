15008910d8d036ae2ec32d21955c79420694ddf4
fuerte <leandrofuerte75@gmail.com>
2025-01-27 09:18:30 +0100

    Listing users

--- index.js
+++ index.js
@@ -3,14 +3,17 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 async function main() {
-  const newUser = await prisma.user.create({
-    data: {
-      email: "maria@gmail.com",
-      name: "Maria",
-      lastname: "Rodriguez",
-    },
-  });
-  console.log(newUser);
+  //   const newUser = await prisma.user.create({
+  //     data: {
+  //       email: "maria@gmail.com",
+  //       name: "Maria",
+  //       lastname: "Rodriguez",
+  //     },
+  //   });
+  //   console.log(newUser);
+
+  const users = await prisma.user.findMany();
+  console.log(users);
 }
 
 main();
