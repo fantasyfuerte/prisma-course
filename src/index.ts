71092b64cb1f708223c6c9f932445a2fe0c77647
fuerte <leandrofuerte75@gmail.com>
2025-01-28 08:55:34 +0100

    meking a migration, creating new product model and seeding it

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log("Hola mundo");
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const products = await prisma.product.create({
+    data: {
+      name: "Phone",
+      price: 89.99,
+      stock: true,
+    },
+  });
+  console.log(products);
+}
+
+main();
