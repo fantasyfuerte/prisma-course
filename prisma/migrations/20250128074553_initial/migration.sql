9a1dec7469c612b8ef6002885716a49e0aa53ca4
fuerte <leandrofuerte75@gmail.com>
2025-01-28 09:24:23 +0100

    Uploading to supabase and making migrations

--- prisma/migrations/20250128074553_initial/migration.sql
+++
@@ -1,7 +0,0 @@
--- CreateTable
-CREATE TABLE "Product" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "name" TEXT NOT NULL,
-    "price" DECIMAL NOT NULL,
-    "stock" BOOLEAN NOT NULL DEFAULT false
-);
